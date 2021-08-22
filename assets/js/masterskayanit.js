2430aa75119dd6e6456885a9bd0254bd293f942e
salaros <salaros@yandex.ru>
2021-08-22 23:55:41 +0300

    removing Instagram feed

--- assets/js/masterskayanit.js
+++ assets/js/masterskayanit.js
@@ -64,20 +64,3 @@ $(document).ready(function () {
         t898_init('193770283');
     }, 500);
 });
-
-(function($){
-    $(window).on('load', function(){
-        $.instagramFeed({
-            'username': 'zhmayeva',
-            'container': "#instagram-feed1",
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'callback': null,
-            'styling': true,
-            'items': 8,
-            'items_per_row': 4,
-            'margin': 1 
-        });
-    });
-})(jQuery);
